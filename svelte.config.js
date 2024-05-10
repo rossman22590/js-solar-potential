e326d6f15e332e96854ff9f0e8301359b0f34dc5
Ross Cohen <rosscohen1990@gmail.com>
2024-05-09 23:42:42 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,25 @@
-/*
- Copyright 2023 Google LLC
+import adapter from '@sveltejs/adapter-node'; // Import the correct adapter
+import preprocess from 'svelte-preprocess'; // Import svelte-preprocess
+import * as child_process from 'node:child_process'; // Import child_process for versioning
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import * as child_process from 'node:child_process';
-
-/** @type {import('@sveltejs/kit').Config} */
+// Define the configuration object
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  // Define preprocessors
+  preprocess: preprocess(),
+
+  // Configure kit options
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    // Configure the adapter, use the correct adapter for your target environment
     adapter: adapter({
-      out: 'public' // Sets the output directory to 'public'
+      out: 'build' // Sets the output directory to 'build'
     }),
 
-    // https://kit.svelte.dev/docs/configuration#version
+    // Add versioning based on the latest git commit
     version: {
       name: child_process.execSync('git rev-parse HEAD').toString().trim(),
-    },
+    }
   },
 };
 
-
+// Export the configuration object as default
+export default config;
