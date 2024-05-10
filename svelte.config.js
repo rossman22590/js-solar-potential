22e14692deb08a68df3ec26aad6b46eba7d205e4
Ross Cohen <rosscohen1990@gmail.com>
2024-05-09 23:41:09 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,43 +1,3 @@
-// /*
-//  Copyright 2023 Google LLC
-
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-
-//       https://www.apache.org/licenses/LICENSE-2.0
-
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//  */
-
-// import adapter from '@sveltejs/adapter-node';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import * as child_process from 'node:child_process';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-//   // for more information about preprocessors
-//   preprocess: vitePreprocess(),
-
-//   kit: {
-//     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-//     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-//     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-//     adapter: adapter(),
-
-//     // https://kit.svelte.dev/docs/configuration#version
-//     version: {
-//       name: child_process.execSync('git rev-parse HEAD').toString().trim(),
-//     },
-//   },
-// };
-
-// export default config;
 /*
  Copyright 2023 Google LLC
 
@@ -63,7 +23,6 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -74,16 +33,9 @@ const config = {
 
     // https://kit.svelte.dev/docs/configuration#version
     version: {
-      name: (() => {
-        try {
-          return child_process.execSync('git rev-parse HEAD').toString().trim();
-        } catch (error) {
-          console.error('Failed to retrieve Git commit hash:', error);
-          return 'unknown';
-        }
-      })(),
+      name: child_process.execSync('git rev-parse HEAD').toString().trim(),
     },
   },
 };
 
-export default config;
+
